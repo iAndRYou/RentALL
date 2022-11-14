8e02343eb5610b02c98eb2bbabb6d14698bc62e6
adrk17 <adrian.krupa17@gmail.com>
2022-11-14 17:16:01 +0100

    MainContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { StyledHeader } from './components/Header.style';
+import {StyledMainContainer} from './components/MainContainer.style';
 
 function App() {
   return (
     <div className='App'>
       <StyledHeader>
       </StyledHeader>
+      <StyledMainContainer>
+      </StyledMainContainer>
     </div>
   )
 }
---
+++ frontend/src/components/MainContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MainContainer({className}) {
+  return (
+    <div className={className}>MainContainer</div>
+  )
+}
