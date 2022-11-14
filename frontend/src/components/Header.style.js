c552a39309d9a2dcbd4b788495d317373c71526a
iAndRYou <akmiecik@student.agh.edu.pl>
2022-11-14 15:39:13 +0100

    1

--- frontend/src/components/Header.style.js
+++ frontend/src/components/Header.style.js
@@ -3,7 +3,19 @@ import Header from './Header';
 
 export const StyledHeader = styled(Header)`
     width: 100vw;
-    height: 200px;
+    height: 100px;
     background-color: blue;
     color: white;
 `;
+
+export const Logo = styled.image`
+    height: 70px;
+    width: 70px;
+    margin: 5px;
+    float: left;
+
+    &:hover {
+        background-color: black;
+    }
+`;
+
