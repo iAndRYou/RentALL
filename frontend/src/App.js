021e4405cd464b7a52b1bd7f48f3d77701e4690a
adrk17 <adrian.krupa17@gmail.com>
2022-11-14 20:52:14 +0100

    colors and more info in ApartmentContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import {StyledMainContainer} from './components/MainContainer.style';
 import {StyledApartmentContainer} from './components/ApartmentContainer.style';
 import logo from "./assets/logo.png";
 import React, {useState} from 'react';
+import Apartments from './components/exampleApartments.json';
 
 function App() {
-  const [apartments, setApartments] = useState(['1000zl', '2000zl', '2000zl', '2000zl', '2000zl']);
-
+  //const [apartments, setApartments] = useState(['1000zl', '2000zl', '2000zl', '2000zl', '2000zl']);
+  //const arr = ['1000zl', '2000zl', '2000zl', '2000zl', '2000zl'];
+ 
   return (
-
     <AppContainer>
       <StyledHeader>
         <Logo src={logo}></Logo>
@@ -19,8 +20,8 @@ function App() {
 
       <StyledMainContainer>
         {
-        apartments.map(price => {
-          return <StyledApartmentContainer price={price}/>
+        Apartments.map(apartment => {
+          return <StyledApartmentContainer price={apartment.price} city={apartment.city} street={apartment.street} picture={apartment.picture} />
         })}
       </StyledMainContainer>
     </AppContainer>
--- frontend/src/components/ApartmentContainer.js
+++ frontend/src/components/ApartmentContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { AppContainer, ApartmentInfo, DefText, BetterText } from './ApartmentContainer.style'
 
-export default function ApartmentContainer({className, price}) {
+export default function ApartmentContainer({className, price, city, street, picture}) {
   return (
-    <div className={className} price={price}>
-        ApartmentContainer
-        <h1>{price}</h1>
+    <div className={className} price={price} city={city} street={street} picture={picture}>
+        <ApartmentInfo>
+            <DefText>cena: </DefText>
+            <BetterText colour='lightgreen'>{price}</BetterText><br></br>
+            <DefText>Lokalizacja: </DefText>
+            <BetterText colour='grey'>{city}, {street}</BetterText>
+        </ApartmentInfo>
     </div>
   )
 }
