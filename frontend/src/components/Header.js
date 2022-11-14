11f91edb0afeec6a4a1e3f221a30bac6a86d9885
iAndRYou <akmiecik@student.agh.edu.pl>
2022-11-14 15:39:15 +0100

    1

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Logo } from "./Header.style";
 
 function Header({className}) {
     return (
         <div className={className}>
-
+            <Logo></Logo>
         </div>
     )
   }
