ff692d05862116a0a8af6df46359ce6ecc6b0f19
adrk17 <adrian.krupa17@gmail.com>
2022-11-14 20:56:37 +0100

    better font size and colors

--- frontend/src/components/ApartmentContainer.js
+++ frontend/src/components/ApartmentContainer.js
@@ -5,9 +5,9 @@ export default function ApartmentContainer({className, price, city, street, pict
   return (
     <div className={className} price={price} city={city} street={street} picture={picture}>
         <ApartmentInfo>
-            <DefText>cena: </DefText>
-            <BetterText colour='lightgreen'>{price}</BetterText><br></br>
-            <DefText>Lokalizacja: </DefText>
+            <DefText>Cena:&nbsp;</DefText>
+            <BetterText colour='#00cc00'>{price}&nbsp;zł / miesiąc</BetterText><br></br>
+            <DefText>Lokalizacja:&nbsp;</DefText>
             <BetterText colour='grey'>{city}, {street}</BetterText>
         </ApartmentInfo>
     </div>
