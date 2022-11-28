91623dc05dbac3e54ceb0568a7fc2207c7e813e4
KrzysztofGG <krzys.gora2@gmail.com>
2022-11-28 23:11:11 +0100

    adverts

--- backend/app/advert.py
+++ backend/app/advert.py
@@ -1,46 +1,37 @@
-from fastapi import APIRouter, Query, Path, Body
+from fastapi import APIRouter, Query, Path, Depends, Body, Form, HTTPException
 from .models import Advert
+from db_user_interface import DatabaseDetails, get_connection
+from db_advert_interface import DBGetAdvert, DBEditAdvert
 from typing import List
 
 
 router = APIRouter()
 
 
-
+#TODO:
 @router.get("/adverts", response_model=List[Advert], tags=['adverts'])
 async def get_adverts(lower_price_bound: float = Query(default=None), upper_price_bound: float = Query(default=None)):
-    """Get adverts from the database"""
-    get_adverts_query = "SELECT * FROM adverts WHERE price >= {0} AND price <= {1}".format(lower_price_bound, upper_price_bound)
-    cursor.execute(get_adverts_query)
-    rows = cursor.fetchall()
-    adverts = []
-    for row in rows:
-        advert = Advert(advert_id=row[0], latitude=row[1], longitude=row[2], date=row[3], price=row[4], author_id=row[5], description=row[6], title=row[7], images=row[8])
-        adverts.append(advert)
-    return adverts
+    if lower_price_bound is None and upper_price_bound is None:
+        return DBGetAdvert.get_all_adverts()
+    elif lower_price_bound is None:
+        return DBGetAdvert.get_adverts_in_given_price(0, upper_price_bound)
+    elif upper_price_bound is None:
+        return DBGetAdvert.get_adverts_in_given_price(lower_price_bound, 1000000000)
+    else:
+        return DBGetAdvert.get_adverts_in_given_price(lower_price_bound, upper_price_bound)
 
 
 
 
 @router.post("/adverts", tags=['adverts'])
 async def post_advert(advert: Advert):
-    """Post advert to the database"""
-    insert_advert_query = "INSERT INTO adverts (latitute, longitude, date, price, author_id, description, title, images) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);" #list dla images
-    cursor.execute(insert_advert_query, (advert.latitude, advert.longitude, advert.date, advert.price, advert.author_id, advert.description, advert.title, advert.images))
-    conn.commit()
+    DBEditAdvert.add_advert(advert)
     return {"message": "Advert added successfully"}
     
 
 
-    
 @router.get("/adverts/{advert_id}", response_model = Advert, tags=['adverts'])
-async def get_advert(advert_id: int = Path()):
-    """Get advert from the database"""
-    get_advert_query = "SELECT * FROM adverts WHERE advert_id = {0}".format(advert_id)
-    cursor.execute(get_advert_query)
-    row = cursor.fetchone()
-    advert = Advert(advert_id=row[0], latitude=row[1], longitude=row[2], date=row[3], 
-    price=row[4], author_id=row[5], description=row[6], title=row[7], images=row[8])
+async def get_advert(advert: Advert = Depends(DBGetAdvert.get_advert_by_id)):
     return advert
 
 
@@ -48,9 +39,7 @@ async def get_advert(advert_id: int = Path()):
 @router.put("/adverts/{advert_id}", tags=['adverts'])
 async def update_user_advert(advert_id: int = Path(), advert: Advert = Body()):
     """Update advert in the database"""
-    update_advert_query = "UPDATE adverts SET latitute = %s, longitude = %s, date = %s, price = %s, author_id = %s, description = %s, title = %s, images = %s WHERE advert_id = {0}".format(advert_id)
-    cursor.execute(update_advert_query, (advert.latitude, advert.longitude, advert.date, advert.price, advert.author_id, advert.description, advert.title, advert.images))
-    conn.commit()
+    DBEditAdvert.update_advert(advert_id, advert)
     return {"message": "Advert updated successfully"}
 
 
@@ -58,9 +47,7 @@ async def update_user_advert(advert_id: int = Path(), advert: Advert = Body()):
 @router.delete("/adverts/{advert_id}", tags=['adverts'])
 async def delete_user_advert(advert_id: int):
     """Delete advert from the database"""
-    delete_advert_query = "DELETE FROM adverts WHERE advert_id = {0}".format(advert_id)
-    cursor.execute(delete_advert_query)
-    conn.commit()
+    DBEditAdvert.delete_advert(advert_id)
     return {"message": "Advert deleted successfully"}
 
 
---
+++ backend/app/db_advert_interface.py
@@ -0,0 +1,84 @@
+
+import psycopg2
+from fastapi import APIRouter, Query, Path, Depends, Body, Form, HTTPException
+from typing import Optional, List
+from .models import Advert
+from db_user_interface import DatabaseDetails, get_connection
+
+db = DatabaseDetails()
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(db.host, db.user, db.dbname, db.password, db.sslmode)
+
+class DBGetAdvert:
+    '''
+    Class for getting adverts from database
+    '''
+
+    @get_connection
+    def get_advert_by_id(cursor, advert_id: int) -> Optional[Advert]:
+        '''
+        Get advert from database by id
+        '''
+        cursor.execute("SELECT * FROM adverts WHERE advert_id = %s;", (advert_id,))
+        rows = cursor.fetchall()
+        if len(rows) == 0:
+            raise HTTPException(status_code=404, detail="Advert not found")
+        advert = Advert(**{
+            "advert_id": rows[0][0],
+            "latitute": rows[0][1],
+            "longitude": rows[0][2],
+            "date": rows[0][3],
+            "price": rows[0][4],
+            "author_id": rows[0][5],
+            "description": rows[0][6],
+            "title": rows[0][7],
+            "images": rows[0][8],
+        })
+
+        return advert
+    
+    def get_adverts_in_given_price(cursor, lower_price_bound: int, upper_price_bound: int) -> Optional[List[Advert]]:
+        '''
+        Get adverts from database in given price
+        '''
+        cursor.execute("SELECT * FROM adverts WHERE price >= %s AND price <= %s;", (lower_price_bound,upper_price_bound))
+        rows = cursor.fetchall()
+        if len(rows) == 0:
+            raise HTTPException(status_code=404, detail="Advert not found")
+        adverts = []
+        for row in rows:
+            advert = Advert(**{
+            "advert_id": row[0],
+            "latitute": row[1],
+            "longitude": row[2],
+            "date": row[3],
+            "price": row[4],
+            "author_id": row[5],
+            "description": row[6],
+            "title": row[7],
+            "images": row[8],
+            })
+            adverts.append(advert)
+            
+        return adverts
+    
+class DBEditAdvert:
+
+    @get_connection
+    def add_advert(cursor, advert: Advert) -> None:
+        cursor.execute("INSERT INTO adverts (latitute, longitude, date, price, author_id, description, title, images) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", 
+        (advert.latitude, advert.longitude, advert.date, advert.price, advert.author_id, advert.description, advert.title, advert.images))
+    
+    @get_connection
+    def delete_advert(cursor, advert_id: int) -> None:
+        cursor.execute("DELETE FROM adverts WHERE advert_id = %s;", (advert_id,))
+
+    @get_connection
+    def update_advert(cursor, advert_id: int, advert: Advert) -> None:
+        cursor.execute("UPDATE adverts SET latitute = %s, longitude = %s, date = %s, price = %s, author_id = %s, description = %s, title = %s, images = %s WHERE advert_id = %s;", 
+        (advert.latitude, advert.longitude, advert.date, advert.price, advert.author_id, advert.description, advert.title, advert.images, advert_id))
+
+        
+
+
+
+
