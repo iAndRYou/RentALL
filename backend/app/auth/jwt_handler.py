68742b8262f4c45b19ac5997876ca1b705b4e6bb
kkasj <kkostowski@student.agh.edu.pl>
2022-11-28 22:08:42 +0100

    AZ CIEZKO WYMIENIC

--- backend/app/auth/jwt_handler.py
+++ backend/app/auth/jwt_handler.py
@@ -1,6 +1,66 @@
 from jose import JWTError, jwt
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from models import User
+from datetime import datetime, timedelta
+from db_user_interface import DBGetUser
+from models import User, TokenData
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+
+def decode_token(token: str) -> User:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise JWTError
+        token_data = TokenData(email=email)
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user = DBGetUser.get_user_by_email(token_data.email)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
 
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(email: str, password: str):
+    user = DBGetUser.get_user_by_email(email)
+    if not user: # no user was found with that email
+        return False
+    if not verify_password(password, user.hashed_password): # password hashes don't match
+        return False
+    return user
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
